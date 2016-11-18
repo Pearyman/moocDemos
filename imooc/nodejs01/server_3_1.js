0bde78b70c3f024ccad7efa170a25e314f7a4a3d
Pearyman <liyn@cim120.com.cn>
2016-11-18 14:49:47 +0800

    update nodejs 3-1

--- imooc/nodejs01/server_3_1.js
+++ imooc/nodejs01/server_3_1.js
@@ -1,9 +1,10 @@
 var http = require('http');
-http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
     res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+});
 
+server.listen(1337, '127.0.0.1');
 console.log('Server is running at http://127.0.0.1:1337');
