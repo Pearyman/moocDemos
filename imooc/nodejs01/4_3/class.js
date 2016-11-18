a62e42a69f1985945f2ff1ab24211fae635f86aa
Pearyman <liyn@cim120.com.cn>
2016-11-18 15:15:27 +0800

    update node 4-3

---
+++ imooc/nodejs01/4_3/class.js
@@ -0,0 +1,10 @@
+var student = require('./student');
+var teacher = require('./teacher');
+
+function add(teacherName, students) {
+    teacher.add(teacherName);
+    students.forEach(function(item, index) {
+        student.add(item);
+    });
+}
+exports.add = add;
---
+++ imooc/nodejs01/4_3/index.js
@@ -0,0 +1,14 @@
+var klass = require('./class');
+
+
+klass.add('Scoot', ['白富美', '高富帅']);
+
+// exports.add = function(klasses) {
+//     klasses.forEach(function(item, index) {
+//         var _klass = item;
+//         var teacherName = item.teacherName;
+//         var students = item.students;
+//
+//         klass.add(teacherName, students);
+//     });
+// };
