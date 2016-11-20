1c9aa67b45f478a6d81c52b060cc7265b3e1fbdd
Pearyman <liyananhappy@sina.cn>
2016-11-20 14:16:18 +0800

    del old folder

--- imooc/nodejs01/5_8/http.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-http
-    .createServer(function(req, res) {
-        res.writeHead(200, {
-            'Content-type': 'text/plain'
-        });
-        res.write('hello world');
-        res.end();
-    }).listen(2016);
