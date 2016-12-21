6d0a366b0ef1d36706da9e2da29f60cbc439f5b5
Pearyman <liyananhappy@sina.cn>
2016-12-21 21:50:55 +0800

    update vue

--- imooc/ele_vuejs/sell/src/components/Hello.vue
+++ imooc/ele_vuejs/sell/src/components/Hello.vue
@@ -1,9 +1,3 @@
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
-</style>
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
@@ -23,3 +17,9 @@ export default {
   }
 }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
