71dfdd11436480c602e52e4ded0bc642ebf569c9
Pearyman <liyananhappy@sina.cn>
2017-03-16 17:22:26 +0800

    add header

--- imooc/vuejs/sell/src/App.vue
+++ imooc/vuejs/sell/src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view></router-view>
+    <div class="header">
+      header区块
+    </div>
+    <div class="tab">
+      tab区块
+    </div>
+    <div class="content">
+      content区块
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
