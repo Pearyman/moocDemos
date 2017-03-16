11506c07d91dfdce34c0a4e634a9362d2a1a20d2
Pearyman <liyananhappy@sina.cn>
2017-03-16 16:36:55 +0800

    add rounter

--- imooc/vuejs/sell/build/dev-server.js
+++ imooc/vuejs/sell/build/dev-server.js
@@ -24,24 +24,51 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 // https://github.com/chimurai/http-proxy-middleware
 var proxyTable = config.dev.proxyTable
 
-var app = express()
-var compiler = webpack(webpackConfig)
+var app = express();
+var appData = require('../static/data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller',function(req,res){
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+apiRoutes.get('/goods',function(req,res){
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+apiRoutes.get('/ratings',function(req,res){
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use('/api', apiRoutes);
+var compiler = webpack(webpackConfig);
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: true
-})
+});
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {}
-})
+});
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({ action: 'reload' })
     cb()
-  })
-})
+  });
+});
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
