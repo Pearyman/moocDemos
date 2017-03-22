8967815c379ca4cf0bb5cde843e6608c5ca424e6
Pearyman <liyananhappy@sina.cn>
2017-03-22 17:28:16 +0800

    update header background

--- imooc/vuejs/sell/src/components/header/header.vue
+++ imooc/vuejs/sell/src/components/header/header.vue
@@ -25,6 +25,9 @@
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -43,8 +46,9 @@
   @import '../../common/stylus/mixin'
 
   .header
+    position: relative
     color: #fff
-    background: #999
+    background: rgba(7,17,27,0.5)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -144,5 +148,12 @@
         font-size: 10px
         right: 12px
         top: 8px
-
+    .background
+      position: absolute
+      top:0
+      left:0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
